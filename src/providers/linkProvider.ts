eafeb35aca99c5785ddbbf2408c534fa5c195002
Muah <ctf0@users.noreply.github.com>
2023-01-04 00:58:57 +0200

    016

--- src/providers/linkProvider.ts
+++ src/providers/linkProvider.ts
@@ -1,50 +1,50 @@
-'use strict'
+'use strict';
 
 import {
     DocumentLink,
     DocumentLinkProvider,
     TextDocument,
-    window
-} from 'vscode'
-import * as util from '../util'
+    window,
+} from 'vscode';
+import * as util from '../util';
 
 export default class LinkProvider implements DocumentLinkProvider {
-    methods: string
+    methods: string;
 
     constructor() {
-        this.methods = util.methods
+        this.methods = util.methods;
     }
 
     async provideDocumentLinks(doc: TextDocument): Promise<DocumentLink[]> {
-        let editor = window.activeTextEditor
+        const editor = window.activeTextEditor;
 
         if (editor) {
-            util.setWs(doc.uri)
+            util.setWs(doc.uri);
 
-            const text = doc.getText()
+            const text = doc.getText();
 
-            let regex   = new RegExp(`(${this.methods})\\((['"][^$]*?['"])`, 'g')
-            let links   = []
-            let matches = text.matchAll(regex)
+            const regex = new RegExp(`(${this.methods})\\((['"][^$]*?['"])`, 'g');
+            const links = [];
+            const matches = text.matchAll(regex);
 
             for (const match of matches) {
-                let method  = match[1]
-                let found   = match[2]
-                let i       = match.index + method.length + 1
-                let files   = await util.getFilePaths(method, found)
-                const range = doc.getWordRangeAtPosition(doc.positionAt(i), new RegExp(found))
+                const method = match[1];
+                const found = match[2];
+                const i = match.index + method.length + 1;
+                const files = await util.getFilePaths(method, found);
+                const range = doc.getWordRangeAtPosition(doc.positionAt(i), new RegExp(found));
 
                 if (files.length && range) {
                     for (const file of files) {
-                        let documentlink     = new DocumentLink(range, file.fileUri)
-                        documentlink.tooltip = file.tooltip
+                        const documentlink = new DocumentLink(range, file.fileUri);
+                        documentlink.tooltip = file.tooltip;
 
-                        links.push(documentlink)
+                        links.push(documentlink);
                     }
                 }
             }
 
-            return links
+            return links;
         }
     }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,86 +1,82 @@
-'use strict'
+'use strict';
 
-import {
-    env,
-    workspace,
-    Uri,
-    window,
-    commands
-} from 'vscode'
 import escapeStringRegexp from 'escape-string-regexp';
+import * as path from 'node:path';
+import {
+    Uri,
+    workspace,
+} from 'vscode';
 
-const path = require('path')
-const sep = path.sep
+const sep = path.sep;
 
-let ws
+let ws;
 
 export function setWs(uri) {
-    ws = workspace.getWorkspaceFolder(uri)?.uri.fsPath
+    ws = workspace.getWorkspaceFolder(uri)?.uri.fsPath;
 }
 
 /* -------------------------------------------------------------------------- */
 
-let cache_store_link = []
+const cache_store_link = [];
 
 export async function getFilePaths(method, text) {
     // allow files only
     // as we don't have an api for revel in sidebar
     if (!text.includes('.')) {
-        return []
+        return [];
     }
 
-    text = text.replace(/['"]/g, '')
+    text = text.replace(/['"]/g, '');
 
-    let cache_key = `${method}_${text}`
-    let list      = checkCache(cache_store_link, cache_key)
+    const cache_key = `${method}_${text}`;
+    let list = checkCache(cache_store_link, cache_key);
 
     if (!list.length) {
-        list = await getData(method, text)
+        list = await getData(method, text);
 
         if (list.length) {
-            saveCache(cache_store_link, cache_key, list)
+            saveCache(cache_store_link, cache_key, list);
         }
     }
 
-    return list
+    return list;
 }
 
 async function getData(method, text) {
-    let paths  = config.list[method]
-    let result = []
+    let paths = config.list[method];
+    const result = [];
 
     // in case a method can have multiple paths config
     if (!Array.isArray(paths)) {
-        paths = [paths]
+        paths = [paths];
     }
 
     for (const path of paths) {
-        let p    = path.replace(/[\\\/]/g, sep)
-        let file = text.replace(/[\\\/]/g, sep)
+        const p = path.replace(/[\\\/]/g, sep);
+        const file = text.replace(/[\\\/]/g, sep);
 
         result.push({
-            tooltip: file,
-            fileUri: Uri.file(normalizePath(`${ws}${p}${sep}${file}`))
-        })
+            tooltip : file,
+            fileUri : Uri.file(normalizePath(`${ws}${p}${sep}${file}`)),
+        });
     }
 
-    return result
+    return result;
 }
 
-function normalizePath(path)
-{
+function normalizePath(path) {
     return path
-            .replace(/\/+/g, '/')
-            .replace(/\+/g, '\\')
+        .replace(/\/+/g, '/')
+        .replace(/\+/g, '\\');
 }
 
 
 /* Helpers ------------------------------------------------------------------ */
 
 function checkCache(cache_store, text) {
-    let check = cache_store.find((e) => e.key == text)
+    const check = cache_store.find((e) => e.key == text);
 
-    return check ? check.val : []
+    return check ? check.val : [];
 }
 
 function saveCache(cache_store, text, val) {
@@ -88,18 +84,18 @@ function saveCache(cache_store, text, val) {
         ? false
         : cache_store.push({
             key : text,
-            val : val
-        })
+            val : val,
+        });
 
-    return val
+    return val;
 }
 
 /* Config ------------------------------------------------------------------- */
-export const PACKAGE_NAME = 'laravelGotoPath'
-export let config: any = {}
-export let methods: any = ''
+export const PACKAGE_NAME = 'laravelGotoPath';
+export let config: any = {};
+export let methods: any = '';
 
 export function readConfig() {
-    config  = workspace.getConfiguration(PACKAGE_NAME)
-    methods = Object.keys(config.list).map((e) => escapeStringRegexp(e)).join('|')
+    config = workspace.getConfiguration(PACKAGE_NAME);
+    methods = Object.keys(config.list).map((e) => escapeStringRegexp(e)).join('|');
 }
